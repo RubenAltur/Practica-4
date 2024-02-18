30676ce318e34350e2f604ab2edc12d0c7b0db6a
Ruben Altur Bononad <rubenaltur2883@gmail.com>
2024-02-18 19:38:24 +0100

    dfd

--- src/factorial.py
+++ src/factorial.py
@@ -8,7 +8,7 @@ def factorial(num):
     """
     if isinstance(num,int):
         if num>0:
-            if num==1 or num==0:
+            if num==1:
                 return 1
             resultat = 1
             for i in range(2, num + 1):
@@ -16,5 +16,4 @@ def factorial(num):
             return resultat
         else:
             raise ValueError("No poden introduir numeros negatius")
-    else:
-        raise TypeError("Sols es poden introduir numeros")
+    raise TypeError("Sols es poden introduir numeros")
